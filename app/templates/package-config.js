f52139a5be40e2cdcdd3ef1763c2b397e5997477
neekey <ni184775761@gmail.com>
2013-07-25 16:30:28 +0800

    1、去掉page下面的版本号 2、更新package-config

--- app/templates/package-config.js
+++ app/templates/package-config.js
@@ -1,7 +1,12 @@
+/*
+ combined files :
+
+ common/package-config
+
+ */
 /**
  * ABC.config({
  *   pageName: 'list',
- *   pageVersion: 'v1',
  *   pub: '1.1.1',
  *   path: 'http://g.tbcdn.cn/myGroup/myRepo/',
  *   charset: 'utf-8'
@@ -10,71 +15,79 @@
  */
 (function (S) {
 
-  window.ABC = window.ABC || {
-    /**
-     * Config Kissy 1.2 packages
-     * of a FrontBuild Page
-     * @param {Object} config
-     * @param config.pageName     name of FrontBuild
-     * @param config.pageVersion  version of you app
-     * @param config.pub      timestamp of published directory
-     * @param config.path     url of you fbapp root
-     * @param config.charset
-     * @param config.debug    debug mode switch
-     */
-    config: function (config) {
-      if (!config.path) {
-        config.path = '';
-      }
+    window.ABC = window.ABC || {
+        /**
+         * Config Kissy 1.2 packages
+         * of a FrontBuild Page
+         * @param {Object} config
+         * @param config.pageName     name of FrontBuild
+         * @param config.pub      timestamp of published directory
+         * @param config.path     url of you fbapp root
+         * @param config.charset
+         * @param config.debug    debug mode switch
+         */
+        config: function (config) {
+            if (!config.path) {
+                config.path = '';
+            }
 
-      var debugPagePath = S.unparam(location.search.replace(/^\?/, ''))['ks-debug'];
+            var debug = config.debug ? true : KISSY.Config.debug;
+            var debugPagePath = S.unparam(location.search.replace(/^\?/, ''))['ks-debug'];
 
-      if (debugPagePath) {
-        config.path = debugPagePath;
-        config.pub = "src/";
-        config.debug = true;
-      }
+            if ( debug ){
+                if( debugPagePath ){
+                    config.path = debugPagePath;
+                    config.pub = "src";
+                }
+                else {
+                    config.pub = 'src';
+                }
 
-      config.path = config.path.replace(/\/$/, '');
+                config.debug = true;
+            }
 
-      var pkgs = [],
-        packageConfig = {},
-        pagePath = S.substitute('{path}/{pub}/pages/{pageName}/{pageVersion}', config),
-      //switch dev or production env
-        debug = config.debug ? true : KISSY.Config.debug;
+            config.path = config.path.replace(/\/$/, '');
 
-      //package config
-      S.mix(packageConfig, config, true, ['charset', 'tag']);
+            var pkgs = [];
+            var packageConfig = {};
+            var pagePath = S.substitute('{path}/{pub}/pages/{pageName}', config);
+            var widgetPath = S.substitute('{path}/{pub}', config);
+            var commonPath = S.substitute('{path}/{pub}', config);
+            var utilsPath = S.substitute('{path}/{pub}', config);
 
-      //common package
-      pkgs.push(S.merge(packageConfig, {
-        name: 'common',
-        path: config.path
-      }));
+            //package config
+            S.mix(packageConfig, config, true, [ 'charset' ]);
 
-      //widget package
-      pkgs.push(S.merge(packageConfig, {
-        name: 'widget',
-        path: config.path
-      }));
+            //common package
+            pkgs.push(S.merge(packageConfig, {
+                name: 'common',
+                path: commonPath
+            }));
 
-      //utils package is only for dev mode
-      if (debug) {
-        pkgs.push(S.merge(packageConfig, {
-          name: 'utils',
-          path: config.path
-        }));
-      }
+            //widget package
+            pkgs.push(S.merge(packageConfig, {
+                name: 'widget',
+                path: widgetPath
+            }));
 
-      //page packages
-      pkgs.push(S.merge(packageConfig, {
-        name: 'page',
-        path: pagePath
-      }));
+            //utils package is only for dev mode
+            if (debug) {
+                pkgs.push(S.merge(packageConfig, {
+                    name: 'utils',
+                    path: utilsPath
+                }));
+            }
 
-      S.config({
-        packages: pkgs
-      });
-    }
-  };
+            //page packages
+            pkgs.push(S.merge(packageConfig, {
+                name: 'page',
+                path: pagePath
+            }));
+
+            S.config({
+                packages: pkgs
+            });
+        }
+    };
 })(KISSY);
+
